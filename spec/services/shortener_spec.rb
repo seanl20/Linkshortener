871dbadd33091aee7d50b4cc144f2b14071efdda
sean <seanl20@yahoo.com>
2025-05-26 10:52:16 +1000

    feat: add rspec and create test

---
+++ spec/services/shortener_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe Shortener do
+  let(:url_1) { "https://www.testurl.com" }
+  let(:url_2) { "https://www.othertesturl.com" }
+
+  it "shorten a given URL to a 7 character lookup code" do
+    shortener = Shortener.new(url_1)
+
+    expect(shortener.lookup_code.length).to eq(7)
+  end
+
+  it "gives each URL its own lookup code" do
+    shortener_1 = Shortener.new(url_1)
+    shortener_2 = Shortener.new(url_2)
+    code_1 = shortener_1.lookup_code
+    code_2 = shortener_2.lookup_code
+
+    expect(code_2).not_to eq(code_1)
+  end
+
+  it "always give the same URL for the same lookup code" do
+    shortener_1 = Shortener.new(url_1)
+    shortener_2 = Shortener.new(url_1)
+    code_1 = shortener_1.lookup_code
+    code_2 = shortener_2.lookup_code
+
+    expect(code_1).to eq(code_2)
+  end
+end
