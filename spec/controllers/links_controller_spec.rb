727a6949076b4bbd7b654995dde5a41b0860ccef
sean <seanl20@yahoo.com>
2025-05-30 10:32:14 +1000

    feat: create links controller

---
+++ spec/controllers/links_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe LinksController, type: :controller do
+  let(:url) { "https://www.testurl.com" }
+
+  it "can shorten a link provided by a user" do
+    request.env["HTTP_ACCEPT"] = "text/javascript"
+    post :create, params: { link: { original_url: url } }
+
+    link = assigns(:link)
+
+    expect(link.original_url).to eq(url)
+    expect(link.valid?).to be true
+    expect(link.lookup_code.length).to eq(7)
+    expect(response).to render_template('create')
+  end
+end
